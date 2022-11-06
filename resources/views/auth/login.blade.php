999081ca5e1a1d7d646c054bccbfbedbc28cfed6
ANUGRAH-PC\Anugrah Wiby Sukmono <anugrahsukmono@gmail.com>
2022-11-06 20:57:27 +0700

    Login update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Persuratan [BETA] | YondaktauKoktanyasaya</title>
+  <title>Persuratan [BETA] | Persuratan</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -14,7 +14,7 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('backend/dist/css/adminlte.min.css')}}">
 </head>
-<body class="hold-transition login-page">
+<body class="hold-transition login-page" style="background-color: yellow;">
 <div class="login-box">
   <!-- /.login-logo -->
   <div class="card card-outline card-primary">
