72dac39528332d50b4f8aa7170ca008c4c7e238a
dryelixir95 <dryelixir95@gmail.com>
2023-01-06 00:39:17 +0700

    updtae anyar

--- resources/views/dosen/table_hasilrapat.blade.php
+++ resources/views/dosen/table_hasilrapat.blade.php
@@ -12,9 +12,7 @@
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{Auth::user()->profile_photo_url}}" class="img-circle elevation-2" alt="{{Auth::user()->name }}">
-        </div>
+         
         <div class="info">
           <a href="#" class="d-block">
           {{ Auth::user()->name }}
